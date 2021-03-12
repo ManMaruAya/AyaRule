4958a2c24d00ff1af079aa68723ba0ac16654dac
ManMaruAya <79196662+ManMaruAya@users.noreply.github.com>
2021-03-13 00:15:58 +0800

    Update bilibili.js

--- bilibili.js
+++ bilibili.js
@@ -35,6 +35,18 @@ let magicJS = MagicJS(scriptName, 'INFO');
           magicJS.logError(`推荐去广告出现异常：${err}`);
         }
         break;
+      // 匹配story模式，用于记录Story的aid
+      case /^https:\/\/app\.bilibili\.com\/x\/v2\/feed\/index\/story\?/.test(magicJS.request.url):
+        try{
+          let obj = JSON.parse(magicJS.response.body);
+          let lastItem = obj['data']['items'].pop();
+          let aid = lastItem['stat']['aid'].toString();
+          magicJS.write(storyAidKey, aid);
+        }
+        catch (err){
+          magicJS.logError(`记录Story的aid出现异常：${err}`);
+        }
+        break;
       // 开屏广告处理
       case /^https?:\/\/app\.bilibili\.com\/x\/v2\/splash\/list/.test(magicJS.request.url):
         try{
@@ -65,6 +77,7 @@ let magicJS = MagicJS(scriptName, 'INFO');
             let tab = obj['data']['tab'].filter((e) =>{return tabList.has(e.id);});
             obj['data']['tab'] = tab;
           }
+        break;
       // 我的页面处理，去除一些推广按钮
       case /^https?:\/\/app\.bilibili\.com\/x\/v2\/account\/mine/.test(magicJS.request.url):
         try{
