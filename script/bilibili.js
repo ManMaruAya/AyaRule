92b036d1246fc5da1329ed0c961316df03690ce9
ManMaruAya <79196662+ManMaruAya@users.noreply.github.com>
2021-03-13 14:43:54 +0800

    Update and rename bilitest.js to bilibili.js

--- script/bilibili.js
+++ script/bilibili.js
@@ -4,12 +4,10 @@ let magicJS = MagicJS(scriptName, 'INFO');
   let body = null;
   if (magicJS.isResponse){
     switch (true){
-      // 标签页处理，如去除会员购等等
+#标签页处理，如去除会员购等等
       case /^https?:\/\/app\.bilibili\.com\/x\/resource\/show\/tab/.test(magicJS.request.url):
         try{
-          // 442 开始为概念版id
           const tabList = new Set([39, 40, 41, 42, 151]);
-          // 102 开始为概念版id
           const bottomList = new Set([177, 178, 179, 181]);
           let obj = JSON.parse(magicJS.response.body);
           if (obj['data']['tab']){
